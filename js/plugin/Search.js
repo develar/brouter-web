677526ed02a4b22e3669fd8f187481c66c0525fb
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-06-03 09:08:25 +0200

    use google api to geocode — much more better because Google can search by incorrect text (hintershoder -> hinterstoder)

--- js/plugin/Search.js
+++ js/plugin/Search.js
@@ -1,10 +1,23 @@
+var geocoder = new google.maps.Geocoder();
+
 BR.Search = L.Control.Search.extend({
     options: {
         //url: 'http://nominatim.openstreetmap.org/search?format=json&q={s}',
-        url: 'http://open.mapquestapi.com/nominatim/v1/search.php?format=json&q={s}',
+        //url: 'http://open.mapquestapi.com/nominatim/v1/search.php?format=json&q={s}',
+        callData: function (text, callResponse) {
+            geocoder.geocode({address: text}, callResponse);
+        },
+        filterJSON: function (response) {
+            var json = {};
+            for (var i = 0, n = response.length; i < n; i++) {
+                var result = response[i];
+                json[result.formatted_address] = L.latLng(result.geometry.location.lat(), result.geometry.location.lng());
+            }
+            return json;
+        },
         jsonpParam: 'json_callback',
         propertyName: 'display_name',
-        propertyLoc: ['lat','lon'],
+        propertyLoc: ['lat', 'lon'],
         markerLocation: false,
         circleLocation: false,
         autoType: false,
